2a2ff8bf5e5d1f7adcdec2715ebf0c6e90fa56fc
whoisltd <tiendat.le129@gmail.com>
2023-06-06 23:02:48 +0700

    update

--- train.py
+++ train.py
@@ -302,8 +302,8 @@ def load_pretrained_model(local_rank, model_path: str = ""):
     ) ### YOUR CODE HERE ###
 
     # Create LoRA model
-    # model = LoraModelForCasualLM(model, lora_config)
-    model = get_peft_model(model, lora_config) # Uncomment this line to use PEFT library instead of your implementation in `lora_layer.py`.
+    model = LoraModelForCasualLM(model, lora_config)
+    # model = get_peft_model(model, lora_config) # Uncomment this line to use PEFT library instead of your implementation in `lora_layer.py`.
     if _is_master_process():
         model.print_trainable_parameters()
 
