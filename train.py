f516909436b89851e682593a083236fe00abeeca
whoisltd <tiendat.le129@gmail.com>
2023-06-06 22:05:16 +0700

    update

--- train.py
+++ train.py
@@ -293,7 +293,7 @@ def load_pretrained_model(local_rank, model_path: str = ""):
     # We will then use the config to initialize a LoraModelForCasualLM with the loaded model. 
     # Then, print the trainable parameters of the model.
 
-    lora_config = LoraConfig(
+    config = LoraConfig(
         r=8,
         lora_alpha=16,
         lora_dropout=0.05,
@@ -302,7 +302,7 @@ def load_pretrained_model(local_rank, model_path: str = ""):
     ) ### YOUR CODE HERE ###
 
     # Create LoRA model
-    model = LoraModelForCasualLM(model, lora_config)
+    model = LoraModelForCasualLM(model, config)
     # model = get_peft_model(model, lora_config) # Uncomment this line to use PEFT library instead of your implementation in `lora_layer.py`.
     if _is_master_process():
         model.print_trainable_parameters()
